a151d6857a2be3f2873e982c1c321ce10a683303
Maxim Bodrov <activer12@mail.ru>
2026-02-01 18:33:21 +0300

    feat: new blocker tests

--- auth_service/tests/conftests.py
+++ auth_service/tests/conftests.py
@@ -1,30 +1,68 @@
 import pytest
 from typing import AsyncGenerator
 
+from fastapi import Request, Response
+from httpx import AsyncClient, ASGITransport
+
 from app.database.repo import DataBase
 from app.database.models import User, Key
 from app.database import Base
+from app.depends.db import get_db
+from core.redis_client import get_redis
+from app.__main__ import app
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
 
 engine = create_async_engine(
-    url='sqlite+aiosqlite:///:memory:',
+    url="sqlite+aiosqlite:///:memory:",
+    echo=True,
     pool_pre_ping=True,
-    echo=True
 )
 test_session = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 
-async def get_test_session() -> AsyncGenerator[AsyncSession]:
+async def get_test_db() -> AsyncGenerator[DataBase]:
     async with test_session() as session:
-        yield session
+        yield DataBase(session)
 
 
 @pytest.fixture(scope='function')
 async def test_db() -> AsyncGenerator[DataBase]:
     async with test_session() as session:
-        yield DataBase(session=session)
+        yield DataBase(session)
+
+
+async def test_redis_client(request: Request):
+    class RedisClientMock:
+        def __init__(self, *args, **kwargs):
+            pass
+
+        async def get(self, *args, **kwargs):
+            return None
+
+        async def set(self, *args, **kwargs):
+            pass
+
+        async def get_dict(self, *args, **kwargs):
+            return {}
+
+        async def set_dict(self, *args, **kwargs):
+            pass
+
+        async def delete(self, *args, **kwargs):
+            pass
+    yield RedisClientMock()
+
+
+@pytest.fixture(scope='module')
+async def test_client() -> AsyncGenerator[AsyncClient]:
+    app.dependency_overrides[get_db] = get_test_db
+    app.dependency_overrides[get_redis] = test_redis_client
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
+        yield client
+        app.dependency_overrides.clear()
 
 
 
